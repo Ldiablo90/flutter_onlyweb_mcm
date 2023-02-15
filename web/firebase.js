d0c1828279fe2065154abbefee4a8e478b2a48df
Shoecave <110222982+Shoecave@users.noreply.github.com>
2023-02-15 15:09:58 +0900

    허허허헛
    이러언

--- web/firebase.js
+++ web/firebase.js
@@ -6,15 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDzhUqitS_UrN9N6G8-85I2UjrDFSCSo9I",
-  authDomain: "maccave-app.firebaseapp.com",
-  projectId: "maccave-app",
-  storageBucket: "maccave-app.appspot.com",
-  messagingSenderId: "680151749830",
-  appId: "1:680151749830:web:7676410143f952edd6b3c7",
-  measurementId: "G-SWX7EYKGW4"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
